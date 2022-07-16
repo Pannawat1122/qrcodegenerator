3931f9247d9550e85f685b1e6a60c31cea0e937b
Pannawat1122 <52130250+Pannawat1122@users.noreply.github.com>
2022-07-16 15:18:21 +0700

    js

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-let btn = document.querySelector(".button");
-let qr_code_element = document.querySelector(".qr-code");
 
+  var qrcode = new QRCode(qr_code_element, {
+    text: `${user_input.value}`,
+    width: 180, //128
+    height: 180,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.H
+  });
 
-btn.addEventListener("click",() => {
-    let user_input = document.querySelector("#input_text");
-    if(user_input.value !=""){
-        if(qr_code_element.childElementCount == 0){
-            generate(user_input);
-        }else{
-            qr_code_element.innerHTML="";
-            generate(user_input);
-        }
-      }
-    }
+  let download = document.createElement("button");
+  qr_code_element.appendChild(download);
 
-})
-function generate(user_input){
-    qr_code_element.style ="";
+  let download_link = document.createElement("a");
+  download_link.setAttribute("download", "qr_code.png");
+  download_link.innerHTML = `Download <i class="fa-solid fa-download"></i>`;
 
-    var qrcode = new QRCode(qr_code_element,{
-        text:`${user_input.value}`,
-        Width:180,
-        height:180,
-        colorDark:"#000",
-        colorLight:"#fff"
-        correctLevel:QRCode.correctLevel.H
-    });
-    qr_code_element.style ="display: block";
+  download.appendChild(download_link);
+
+  let qr_code_img = document.querySelector(".qr-code img");
+  let qr_code_canvas = document.querySelector("canvas");
+
+  if (qr_code_img.getAttribute("src") == null) {
+    setTimeout(() => {
+      download_link.setAttribute("href", `${qr_code_canvas.toDataURL()}`);
+    }, 300);
+  } else {
+    setTimeout(() => {
+      download_link.setAttribute("href", `${qr_code_img.getAttribute("src")}`);
+    }, 300);
+  }
 }
+
+generate({
+  value: "https://murtuzaalisurti.github.io/qr"
+});
+
